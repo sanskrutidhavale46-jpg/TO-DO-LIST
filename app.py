c9a5852aa19bcb9ee05d50483217bb50020d78bd
Sanskruti Dhavale <sanskrutidhavale46@gmail.com>
2026-08-08 13:46:01 +0530

    added navbar in base.html and index.html

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+todos = [
+    {"sno":1,"title":"sample task","desc":"this is a sample task for todo list","status":"pending"}
+]
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=8000)
